db26e4c55b03da82123f987f398aaf1f0b72635b
sofiialives <127687828+sofiialives@users.noreply.github.com>
2023-09-13 00:51:21 +0300

    1-2 html

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,8 @@
+import { Profile } from './Profile';
+import user from 'user.json';
+import { Statistics } from './Statistics';
+import data from 'data.json';
+
 export const App = () => {
   return (
     <div
@@ -7,10 +12,20 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+
+      <Statistics title="Upload stats" stats={data}/>
     </div>
   );
 };
